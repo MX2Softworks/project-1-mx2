453b1e032e536d1b119ad25dfb0e1ff01fe00288
Michael Teddick <teddickm@gmail.com>
2016-06-17 01:16:09 -0400

    Added the Reticle Pawn
    Created the reticle pawn that will come under control of the player in
    combat.

---
+++ ProjectOne/Source/ProjectOne/Reticle.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectOne.h"
+#include "Reticle.h"
+
+
+// Sets default values
+AReticle::AReticle()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AReticle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AReticle::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AReticle::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
